e54a037860fdd8f9c18902675431a12d1221f9c8
SHRESHTH YT <shreshthrajvijaykumarsah@outlook.com>
2023-01-05 22:36:49 +0530

    update

--- palindrome.js
+++ palindrome.js
@@ -1,20 +1,4 @@
 function checkPalindrome(N, str) {
-    var name=""
-    var namy=""
-    for(var i=N-1;i>=0;i--){
-        // console.log(str[i])
-        name=name+str[i]
-    }
-    for(var j=0;j<N;j++){
-        namy=namy+str[j]
-    }
-    // console.log(name)
-    // console.log(namy)
-    if(name==namy){
-        console.log('Yes')
-    }
-    else{
-        console.log('No')
-    }
+   //MAYANK HELP ME TO SOLVE THIS QUESTION
     }
     
